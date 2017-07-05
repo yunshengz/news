c13b8a73c15153fcd38453200f5e42d04d9443c2
504461483@qq.com <zhangyunsheng@gongchang.com>
2017-07-05 14:36:17 +0800

    增加无限加载功能

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,19 +1,23 @@
 Page({
   data: {
       newsList:[],
-      num: 1
+      num: 1,
+      add: 1
   },
+  //跳转找新闻详情页
   gotoInfo: function(e) {
       var id = e.currentTarget.dataset.id;
       wx.navigateTo({
         url: '/pages/info/info?news_id='+id,
       });
   },
+  //新闻类型切换
   goOtherCate: function(e) {
      var that = this,
          index = e.currentTarget.dataset.index;
      this.setData({
-        num: index
+        num: index,
+        add: 1
      }),
      wx.request({
        url: 'http://api.dagoogle.cn/news/get-news?pagesize=10&tableNum='+index,
@@ -28,6 +32,29 @@ Page({
        }
      });
   },
+  addMore: function(e) {
+      var that = this,
+          next = this.data.add + 1,
+          url = 'http://api.dagoogle.cn/news/get-news?pagesize=10&tableNum=' + this.data.num +'&page='+next;
+
+      this.setData({
+          add: next
+      });
+
+      wx.request({
+        url: url,
+        header: {
+          'content-type': 'application/json'
+        },
+        success: function (r) {
+          that.setData({
+            newsList: that.data.newsList.concat(r.data.data)
+          });
+          console.log(that.data.newsList);
+        }
+      }); 
+      
+  },
   onLoad: function (options) {
     // Do some initialize when page load.
     var that = this;
@@ -41,8 +68,10 @@ Page({
           that.setData({
             newsList: r.data.data
           });
+          
       }
     });
+    
   },
   onReady: function () {
     // Do something when page ready.
