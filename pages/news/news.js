e6ca9779b82bf3a7e4a50c470beda776db96ba5a
504461483@qq.com <zhangyunsheng@gongchang.com>
2017-07-06 18:29:01 +0800

    增加页面滑动效果

--- pages/news/news.js
+++ pages/news/news.js
@@ -1,8 +1,17 @@
 Page({
   data: {
       newsList:[],
-      num: 1,
-      add: 1
+      open: false,//是否能滑动的标示
+      num: 1,//新闻类型
+      add: 1,//加载第几页的新闻
+      mark: 0,
+      newMark: 0,
+      startMark: 0,
+      endMark: 0,
+      status: 1, //向右滑动标志，向左为0
+      toRight: true,
+      windowWidth: wx.getSystemInfoSync().windowWidth,
+      translate: ''
   },
   //跳转找新闻详情页
   gotoInfo: function(e) {
@@ -60,6 +69,101 @@ Page({
   goTop: function(e) {
       //没有合适的方法
   },
+
+  /* 已下为页面右滑呼出其他信息 */
+  slideStart: function(e) {
+      this.data.mark = this.data.newMark = e.touches[0].pageX;
+
+      this.data.startMark = e.touches[0].pageX;
+
+  },
+  slideMove: function(e) {
+      var that = this,
+          xNum;
+     
+      this.data.newMark = e.touches[0].pageX;
+
+      xNum = this.data.newMark - this.data.startMark;
+      //console.log(xNum);
+
+
+      if(this.data.mark < this.data.newMark){
+
+
+        if (this.data.windowWidth * 0.75 > Math.abs(xNum)){
+
+            this.setData({
+              translate: 'transform: translateX(' + xNum + 'px)'
+            });
+
+        } else {
+
+            this.setData({
+              translate: 'transform: translateX(' + that.data.windowWidth * 0.75 + 'px)'
+            });
+        }
+      }
+    
+
+      // if (this.data.mark > this.data.newMark) {
+          
+      //     this.setData({
+      //       translate: 'transform: translateX(' + (-xNum) + 'px)'
+      //     }); 
+                  
+      // }
+      //console.log(xNum);
+      //this.data.mark = this.data.newMark;
+
+  },
+  slideEnd: e => {
+
+  },
+
+
+
+
+
+
+
+
+
+  //这种呼出效果比较生硬，适合点击某一个按钮后弹出效果；
+  // slideStart: function(e) {
+  //     this.data.mark = this.data.newMark = e.touches[0].pageX;
+  //     //console.log(e);
+  // },
+  // slideMove: function(e) {
+  //     var that = this;
+  //     this.data.newMark = e.touches[0].pageX;
+  //     //右滑
+  //     if(this.data.mark < this.data.newMark) {
+  //         that.setData({
+  //             toRight: true
+  //         });
+  //     }
+  //     //左滑
+  //     if(this.data.mark > this.data.newMark) {
+  //         that.setData({
+  //             toRight: false
+  //         });
+  //     }
+  //     this.data.mark = this.data.newMark;
+  // },
+  // slideEnd: function(e) {
+  //     this.data.mark = this.data.newMark = 0;
+  //     if(this.data.toRight) {
+  //         this.setData({
+  //             open: true
+  //         });
+  //     } else {
+  //         this.setData({
+  //             open: false
+  //         })
+  //     }
+  // },
+  /* 已上为页面右滑呼出其他信息 */
+
   //监听页面加载
   onLoad: function (options) {
     // Do some initialize when page load.
